570567121b0085fcbe22f4104aacf6e68fb9ac18
bucketsize <bucket.size@gmail.com>
2012-08-26 13:13:02 +0530

    comment

--- src/org/heavywater/affector/resolver/AffectorResolver.java
+++ src/org/heavywater/affector/resolver/AffectorResolver.java
@@ -5,6 +5,10 @@ import org.heavywater.property.Dynamics;
 import org.heavywater.property.ElectoStaticProperty;
 import org.heavywater.property.Kinetics;
 
+/**
+ * @author jb
+ *
+ */
 public abstract class AffectorResolver {
 	public abstract Affector resolve(Dynamics dynamics);
 	public abstract Affector resolve(Kinetics kinetics);

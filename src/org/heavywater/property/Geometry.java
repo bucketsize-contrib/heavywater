bdaa8f0b614e90e4b72c97564b5b929030b1f3a3
bucketsize <bucket.size@gmail.com>
2012-09-28 06:53:27 +0530

    new classes

---
+++ src/org/heavywater/property/Geometry.java
@@ -0,0 +1,30 @@
+package org.heavywater.property;
+
+import org.heavywater.affector.Affector;
+import org.heavywater.affector.resolver.AffectorResolver;
+import org.heavywater.ptypes.Mesh;
+import org.heavywater.util.Resolver;
+
+public class Geometry extends Property {
+
+	Mesh mesh;
+	
+	@Override
+	public Affector dispatch(AffectorResolver afr) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object dispatch(Resolver afr) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String inspect() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/org/heavywater/ptypes/Mesh.java
@@ -0,0 +1,5 @@
+package org.heavywater.ptypes;
+
+public class Mesh {
+
+}
